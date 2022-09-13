3adc6a4dadf8f92607882e90f85253ce42acd146
kgeri <gergo.kocsis@stud.uni-corvinus.hu>
2022-09-13 10:58:06 +0200

    merge conflict

--- merge conflict.py
+++ merge conflict.py
@@ -1,4 +1,4 @@
-for i in range(10):
-    print(f"A választott szám: {i}")
-
-    print("ciklus vége")
+for i in range(20):
+    print(f"Az általam választott szám: {i}")
+#Ciklus vége
+    print("ciklus vége:)")
